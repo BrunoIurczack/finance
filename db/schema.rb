0a8adbe3d70a2e042b589c635a195cc1389089c7
Bruno Felipe <bruno@matheuss-MacBook-Air-2.local>
2023-07-12 16:16:49 -0300

    create db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_170421) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "type"
+    t.float "value"
+    t.string "description"
+    t.date "transaction_date"
+    t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

4a370f0c3410ee6040c3a13fac76436f5655e670
Bruno Felipe <bruno@matheuss-MacBook-Air-2.local>
2023-07-17 12:14:30 -0300

    Erro delete

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,5 @@
 import { Application } from "@hotwired/stimulus"
+import Rails from '@rails/ujs'
 
 const application = Application.start()
 
@@ -7,3 +8,5 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+Rails.start();

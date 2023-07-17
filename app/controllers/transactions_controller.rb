575b304d6be39397b3e7a7069a839f23857bd2b3
Bruno Felipe <bruno@matheuss-MacBook-Air-2.local>
2023-07-17 10:18:14 -0300

    Create Edit, New e Form

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,13 +1,11 @@
 class TransactionsController < ApplicationController
-    #before_action :set_transaction, only: %i[ show ]
+  before_action :set_transaction, only: %i[ show edit update destroy ]
 
   def index
     @transactions = Transaction.all
   end
 
-  def show 
-    @transaction = Transaction.find(params[:id])
-  end
+  def show; end
 
   def new
     @transaction = Transaction.new
@@ -26,14 +24,25 @@ class TransactionsController < ApplicationController
   end
 
   def update
+    if @transaction.update(transaction_params)
+      redirect_to transaction_url(@transaction), notice: "Transaction was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @transaction.destroy
+    redirect_to transactions_path
   end
 
   private
-  def set_transaction
-    @transaction = Transaction.find(params[:id])
-  end
+    def set_transaction
+      @transaction = Transaction.find(params[:id])
+    end
+
+    def transaction_params
+      params.require(:transaction).permit(:transaction_type, :value, :description, :transaction_date, :status)
+    end
 
 end
